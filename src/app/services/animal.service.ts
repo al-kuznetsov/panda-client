94e1a00d158371a58c9887790f1939595643bcec
al-kuznetsov <alkuznetsov@aol.com>
2022-11-06 18:23:25 +0500

    Add pagination support to animal-service

--- src/app/services/animal.service.ts
+++ src/app/services/animal.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Animal } from '../common/animal';
 
@@ -9,11 +9,17 @@ import { Animal } from '../common/animal';
 })
 export class AnimalService {
 
-  private baseUrl: string = `${environment.apiBaseUrl}${environment.animalsMappingUrl}`;
+  private baseUrl: string = `${environment.apiBaseUrl}${environment.animalsMappingUrl}?size=100`;
 
   constructor(private httpClient: HttpClient) { }
 
   getAnimalList(): Observable<Animal[]> {
-    return this.httpClient.get<Animal[]>(this.baseUrl);
+    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+      map(response => response.content)
+    )
   }
+}
+
+interface GetResponse {
+  content: Animal[];
 }
