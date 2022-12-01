253aae30c22b9d76e0b5bea743db48ce82d09d22
al-kuznetsov <alkuznetsov@aol.com>
2022-12-01 21:02:29 +0500

    Rework AnimalIndicators

--- src/app/common/animal-indicators.ts
+++ src/app/common/animal-indicators.ts
@@ -1,9 +1,21 @@
 export class AnimalIndicators {
 
-    constructor(public stress: number,
-        public sickness: number,
-        public trauma: number,
-        public mobility: number,
-        public tameness: number
+    constructor(
+        public id: number,
+        public ag: number,
+        public isInfant: boolean,
+        public consciousnessLevel: string,
+        public height: number,
+        public breathingRate: number,
+        public heartRate: number,
+        public bleedingLevel: string,
+        public bodyTemperature: number,
+        public severeDamageCount: number,
+        public mildDamageCount: number,
+        public mobilityLossLevel: string,
+        public appetiteLevel: string,
+        public hasSymptoms: boolean,
+        public isPregnant: boolean,
+        public aggressionLevel: string
     ) { }
 }
