11fffb7e63aa486c015d05737c1825dffdf3162c
al-kuznetsov <alkuznetsov@aol.com>
2022-12-07 22:39:15 +0500

    Change animal-form component
    Add input fields

--- src/app/components/animal-form/animal-form.component.ts
+++ src/app/components/animal-form/animal-form.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { AnimalType } from 'src/app/common/animal-type';
+import { AggressionLevel } from 'src/app/enums/aggression-level';
+import { AppetiteLevel } from 'src/app/enums/appetite-level';
+import { BleedingLevel } from 'src/app/enums/bleeding-level';
+import { ConcsiousnessLevel } from 'src/app/enums/concsiousness-level';
+import { MobilityLossLevel } from 'src/app/enums/mobility-loss-level';
 import { AnimalService } from 'src/app/services/animal.service';
 import { CommonFormService } from 'src/app/services/common-form.service';
 
@@ -9,17 +14,25 @@ import { CommonFormService } from 'src/app/services/common-form.service';
   templateUrl: './animal-form.component.html',
   styleUrls: ['./animal-form.component.css']
 })
-export class AnimalFormComponent implements OnInit {
+export class AnimalFormComponent implements OnInit, AfterViewInit {
 
   animalFormGroup!: FormGroup;
   animalTypes: AnimalType[] = [];
 
+  concsiousnessLevelEnum = ConcsiousnessLevel;
+  bleedingLevelEnum = BleedingLevel;
+  mobilityLevelEnum = MobilityLossLevel;
+  appetiteLevelEnum = AppetiteLevel;
+  aggressionLevelEnum = AggressionLevel;
+
   // Inject the form builder here
   constructor(private formBuilder: FormBuilder,
     private animalService: AnimalService,
-    private commonFormService: CommonFormService) { }
+    private commonFormService: CommonFormService) {
+  }
 
   ngOnInit(): void {
+
     // Get data for drop-down lists
     this.listAnimalTypes();
 
@@ -31,9 +44,31 @@ export class AnimalFormComponent implements OnInit {
         description: [''],
         fullBio: [''],
         active: { value: true, disabled: true },
-        animalType: [{} as AnimalType]
+        animalType: [{} as AnimalType],
+        age: [0],
+        isInfant: [false],
+        consciousnessLevel: [this.concsiousnessLevelEnum.CONSCIOUS],
+        height: [0.00],
+        breathingRate: [0],
+        heartRate: [0],
+        bleedingLevel: [this.bleedingLevelEnum.NO],
+        bodyTemperature: [0.00],
+        severeDamageCount: [0],
+        mildDamageCount: [0],
+        mobilityLossLevel: [this.mobilityLevelEnum.NO],
+        appetiteLevel: [this.appetiteLevelEnum.GOOD],
+        hasSymptoms: [false],
+        isPregnant: [false],
+        aggressionLevel: [this.aggressionLevelEnum.NO]
       })
     })
+  }
+
+  ngAfterViewInit(): void {
+    // TODO: fix it
+    const defaultAnimalType: AnimalType = this.animalTypes[0];
+    console.log(`defaultAnimalType in animal-form is: ${defaultAnimalType}`)
+    this.animalFormGroup.get("animal")?.get("animalType")?.setValue(defaultAnimalType);
   }
 
   listAnimalTypes() {
